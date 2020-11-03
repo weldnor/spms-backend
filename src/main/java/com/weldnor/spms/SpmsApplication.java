7a5aebcc36de65f18bb7be6de7bfb4b6ed97ae23
Kolesov Anton <kolesov.anton.s@gmail.com>
2020-11-03 15:42:05 +0300

    First commit

---
+++ src/main/java/com/weldnor/spms/SpmsApplication.java
@@ -0,0 +1,13 @@
+package com.weldnor.spms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpmsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/weldnor/spms/SpmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weldnor.spms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
