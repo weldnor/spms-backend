da1be1845ee7593b02488d6063d07842b0e1d52e
weldnor <kolesov.anton.s@gmail.com>
2020-12-14 17:58:06 +0300

    Add modelmapper

---
+++ src/main/java/com/weldnor/spms/config/ApplicationConfig.java
@@ -0,0 +1,21 @@
+package com.weldnor.spms.config;
+
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class ApplicationConfig {
+    @Bean
+    public ModelMapper modelMapper() {
+        ModelMapper mapper = new ModelMapper();
+        mapper.getConfiguration()
+                .setMatchingStrategy(MatchingStrategies.STRICT)
+                .setFieldMatchingEnabled(true)
+                .setSkipNullEnabled(true)
+                .setFieldAccessLevel(org.modelmapper.config.Configuration.AccessLevel.PRIVATE);
+        return mapper;
+    }
+}
